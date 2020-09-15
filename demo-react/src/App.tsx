787e32671cec629ef68a2fdcf2596045743dc31d
杨天 <1042164333@qq.com>
2020-09-15 18:45:12 +0800

    新增弹窗ok

--- demo-react/src/App.tsx
+++ demo-react/src/App.tsx
@@ -1,23 +1,21 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import { Timeline, Card, DatePicker, Row, Col, Statistic } from 'antd';
+import React, { useState, useEffect, } from 'react';
+import { Timeline, Card, DatePicker, Row, Col, Statistic, Modal, Form, Select, Button, InputNumber, Switch } from 'antd';
 import axios from 'axios'
+import { PlusCircleOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import 'moment/locale/zh-cn';
 import locale from 'antd/es/date-picker/locale/zh_CN';
+import moment from 'moment';
 import './App.css';
 
-function getdate(timestamp: number): string {
-  var now = new Date(timestamp),
-    y = now.getFullYear(),
-    m = now.getMonth() + 1,
-    d = now.getDate();
-  return y + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d) + " " + now.toTimeString().substr(0, 8);
+interface CategoriesModel {
+  id: string;
+  type: string;
+  name: string;
 }
 
 function App() {
-  let categoriesModel = [
-    { id: '', type: '', name: '' }
-  ];
+  let categoriesModel: Array<CategoriesModel> = [{ id: '', type: '', name: '' }];
   let billModel = [
     {
       key: '',
@@ -32,65 +30,71 @@ function App() {
     },
   ];
   const types = ['支出', '收入']
-  let [categories, setCategories] = useState(categoriesModel)
-  let [dataSource, setDataSource] = useState(billModel)
-  // const [total, dispatch] = useReducer((state: any, action: any) => {
-  //   switch (action.type) {
-  //     case 'earning':
-  //       return dataSource.reduce((total, curr) => {
-  //         total.amount += (curr.isShow && curr.type) === '0' ? curr.amount : 0
-  //         return total
-  //       }).amount
+  const [categories, setCategories] = useState(categoriesModel)
+  const [dataSource, setDataSource] = useState(billModel)
+  const [total, setTotal] = useState({ earning: 0, expenditure: 0 })
+  const [showFormModel, setShowFormModel] = useState(false)
+  const [filterParams, setFilterParams] = useState({ time: '', categorie: '' })
 
-  //     case 'expenditure':
-  //       return dataSource.reduce((total, curr) => {
-  //         total.amount += (curr.isShow && curr.type) === '1' ? curr.amount : 0
-  //         return total
-  //       }).amount
-  //     default:
-  //       return state;
-  //   }
-  // }, [])
-
+  // 初始化
   useEffect(() => {
     (async () => {
-      let categoriesRes = await axios(
-        'http://127.0.0.1:5000/categories',
-      )
-      setCategories(categoriesRes.data)
-      let billRes = await axios(
-        'http://127.0.0.1:5000/bill',
-      )
-      setDataSource(billRes.data.sort((a: any, b: any) => a.time - b.time).map((e: any, i: number) => {
+      categoriesModel = (await axios('http://127.0.0.1:5000/categories',)).data
+      setCategories(categoriesModel)
+      billModel = (await axios('http://127.0.0.1:5000/bill',)).data
+      setDataSource(billModel.sort((a, b) => parseFloat(b.time) - parseFloat(a.time)).map((e, i) => {
         return {
           ...e,
           key: i.toString(),
-          timeStr: getdate(Number(e.time)).replace('00:00:00', ''),
-          categoryStr: categoriesRes.data.filter((se: any) => se.id === e.category)[0].name,
-          typeStr: types[e.type],
+          timeStr: moment(parseInt(e.time)).format('YYYY-MM-DD HH:mm:ss').replace('00:00:00', ''),
+          categoryStr: categoriesModel.filter(se => se.id === e.category)[0].name,
+          typeStr: types[parseInt(e.type)],
           isShow: true
         }
       }));
-      console.log(billRes.data)
+      setTotal({
+        expenditure: [...billModel.filter(e => e.type === '0').map(e => parseFloat(e.amount)), 0].reduce((t, c) => t += c),
+        earning: [...billModel.filter(e => e.type === '1').map(e => parseFloat(e.amount)), 0].reduce((t, c) => t += c),
+      })
     })()
   }, []);
 
-  function onChange(date: any, dateString: string) {
-    setDataSource(dataSource.map(e => (e.isShow = e.timeStr.includes(dateString), e)))
-    console.log(dataSource.filter(e => e.isShow && e.type === '0').map(e => parseFloat(e.amount)))
-  }
-
+  // 筛选 回调
+  useEffect(() => {
+    setDataSource(dataSource.map(e => (e.isShow = true, e)).map(e => {
+      if (filterParams.time)
+        e.isShow = e.timeStr.includes(filterParams.time)
+      if (filterParams.categorie)
+        e.isShow = e.isShow && e.category === filterParams.categorie
+      return e
+    }))
+    setTotal({
+      expenditure: [...dataSource.filter(e => e.isShow && e.type === '0').map(e => parseFloat(e.amount)), 0].reduce((t, c) => t += c),
+      earning: [...dataSource.filter(e => e.isShow && e.type === '1').map(e => parseFloat(e.amount)), 0].reduce((t, c) => t += c),
+    })
+  }, [filterParams])
   return (
     <div className="site-card-border-less-wrapper">
-      <Card bordered={false} style={{ minHeight: 300 }}>
-        <Row gutter={16} style={{ paddingBottom: 20 }}>
-          <Col span={8} style={{ textAlign: "center", verticalAlign: "center" }}>
-            <DatePicker onChange={onChange} picker="month" locale={locale} />
+      <Card bordered={false} >
+        <Row gutter={16} style={{ paddingBottom: 20, marginBottom: 8, position: 'sticky', top: 0, backgroundColor: 'white', zIndex: 1 }}>
+          <Col span={8} style={{ textAlign: "center", alignSelf: 'center' }}>
+            <DatePicker onChange={(date: any, dateString: string) => setFilterParams({ ...filterParams, time: dateString })} picker="month" locale={locale} style={{ marginRight: 10 }} allowClear />
+            <Select placeholder='请选择账单分类' style={{ width: 150 }} onChange={(e: any) => setFilterParams({ ...filterParams, categorie: e })} allowClear>
+              {categories.map(e => (
+                <Select.Option key={e.id} value={e.id}>{e.name}</Select.Option>
+              ))}
+            </Select>
           </Col>
-          {/* <Col span={8} style={{ textAlign: "center" }}><Statistic title="支出" value={dataSource.filter(e => e.isShow && e.type === '0').map(e => parseFloat(e.amount)).reduce((total, curr) => total += curr)} /></Col>
-          <Col span={8} style={{ textAlign: "center" }}><Statistic title="收入" value={dataSource.filter(e => e.isShow && e.type === '1').map(e => parseFloat(e.amount)).reduce((total, curr) => total += curr)} /></Col> */}
+          <Col span={8} style={{ textAlign: "center" }}><Statistic title="总支出金额" value={total.expenditure} /></Col>
+          <Col span={8} style={{ textAlign: "center" }}><Statistic title="总收入金额" value={total.earning} /></Col>
         </Row>
         <Timeline mode='alternate'>
+          <Timeline.Item
+            position={'left'}
+            dot={<PlusCircleOutlined style={{ fontSize: '24px' }} onClick={() => setShowFormModel(true)} />}
+            style={{ marginBottom: 20 }}
+          >
+          </Timeline.Item>
           {
             dataSource.map((e, i) =>
               e.isShow ? (
@@ -98,7 +102,6 @@ function App() {
                   key={i}
                   position={e.type === '0' ? 'left' : 'right'}
                   color={e.type === '0' ? 'red' : 'green'}
-                  dot={null}
                 >
                   <div>{e.timeStr}</div>
                   <div>{`${e.categoryStr}  ${e.amount}元`}</div>
@@ -108,8 +111,79 @@ function App() {
           }
         </Timeline>
       </Card>
+      <FormModel isShow={showFormModel} setShow={setShowFormModel} categories={categories}></FormModel>
     </div >
   );
 }
+
+function FormModel({ isShow, setShow, categories }: { isShow: boolean, setShow: Function, categories: Array<CategoriesModel> }) {
+  let dataSource = { time: '', type: '0', category: '', amount: '' }
+  // 提交新增账单
+  const onFinish = async (values: any) => {
+    console.log(values)
+    await axios.post('http://127.0.0.1:5000/updateBill', { ...values, firstName: 'fred' })
+    // axios.post('/user', {
+    //   firstName: 'Fred',
+    //   lastName: 'Flintstone'
+    // })
+  };
+  return (
+    <Modal
+      title="新增账单"
+      visible={isShow}
+      onCancel={() => setShow(false)}
+      footer={null}
+    >
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="时间"
+          name="time"
+          rules={[{ required: true, message: '请选择时间!' }]}
+        >
+          <DatePicker showTime placeholder='请选择时间' onChange={(e) => { if (e) dataSource.time = e.format('YYYY-MM-DD HH:mm:ss') }} />
+        </Form.Item>
+        <Form.Item
+          label="账单类型"
+          name="type"
+          rules={[{ required: false, message: '请选择账单类型!' }]}
+          valuePropName='type'
+        >
+          <Switch checkedChildren="支出" unCheckedChildren="收入" defaultChecked onChange={(e) => { if (e) dataSource.type = e ? "1" : "0" }} />
+        </Form.Item>
+        <Form.Item
+          label="账单分类"
+          name="category"
+          rules={[{ message: '请选择账单分类!' }]}
+        >
+          <Select placeholder='请选择账单分类' style={{ width: 150 }} onChange={(e) => { if (e) dataSource.category = e.toString() }}>
+            {categories.map(e => (
+              <Select.Option key={e.id} value={e.id}>{e.name}</Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="金额"
+          name="amount"
+          rules={[{ required: true, message: '请输入金额!' }]}
+        >
+          <InputNumber
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={(value = '') => value.replace(/\$\s?|(,*)/g, '')}
+            onChange={(e) => { if (e) dataSource.amount = e.toString() }}
+            precision={2}
+          />
+        </Form.Item>
+        <Form.Item {...{ wrapperCol: { offset: 8, span: 16 }, }}>
+          <Button type="primary" htmlType="submit">提交</Button>
+        </Form.Item>
+      </Form>
+    </Modal >
+  )
+}
+
 
 export default App;
--- python/api.py
+++ python/api.py
@@ -1,28 +1,17 @@
-from flask import Flask
+import flask
 # import request
 from flask_cors import CORS
 import json
 import read
+import write
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 CORS(app, supports_credentials=True)
 
 
 @app.route("/bill", methods=["GET"])
 def bill():
-    # 默认返回内容
     return_dict = read.getData('bill')
-    # 判断入参是否为空
-    # if request.args is None:
-    #     return_dict['return_code'] = '5004'
-    #     return_dict['return_info'] = '请求参数为空'
-    #     return json.dumps(return_dict, ensure_ascii=False)
-    # 获取传入的params参数
-    # get_data = request.args.to_dict()
-    # name = get_data.get('name')
-    # age = get_data.get('age')
-    # 对参数进行操作
-    # return_dict['result'] = tt(name, age)
     return json.dumps(return_dict)
 
 
@@ -32,10 +21,11 @@ def categories():
     return json.dumps(return_dict)
 
 
-# 功能函数
-def tt(name, age):
-    result_str = "%s今年%s岁" % (name, age)
-    return result_str
+@app.route("/updateBill", methods=["POST"])
+def updateBill():
+    print('updateBill', flask.request.values.get('firstName'))
+    return_dict = write.writeData([])
+    return json.dumps(return_dict)
 
 
 if __name__ == "__main__":
